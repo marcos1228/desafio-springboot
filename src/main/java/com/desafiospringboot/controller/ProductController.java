249ba2fb6e13afaa195ba25d9385d0671c8f1484
Marcos Barbosa Evangelista <evangelistamarcos99@gmail.com>
2021-04-07 11:18:19 -0300

    Implementação do endpoint salvar

--- src/main/java/com/desafiospringboot/controller/ProductController.java
+++ src/main/java/com/desafiospringboot/controller/ProductController.java
@@ -1,16 +1,35 @@
 package com.desafiospringboot.controller;
 
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.desafiospringboot.model.Product;
+import com.desafiospringboot.service.ProductService;
 
 @RestController
 @RequestMapping
 public class ProductController {
-
-	@GetMapping("/products")
-	public String buscandoPorId() {
-		return "Teste buscando por ID";
-	}
 	
+	@Autowired
+	private ProductService productService;
+
+	@PostMapping("/products")
+	public ResponseEntity<?> salvar(@RequestBody Product product) {
+		Product obj = productService.insert(product);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+//	
+//	@GetMapping("/products")
+//	public String salvar() {
+//		return "Testando";
+//	}
 }
--- src/main/java/com/desafiospringboot/service/ProductService.java
+++ src/main/java/com/desafiospringboot/service/ProductService.java
@@ -1,8 +1,19 @@
 package com.desafiospringboot.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.desafiospringboot.model.Product;
+import com.desafiospringboot.repository.ProductRepository;
 
 @Service
 public class ProductService {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	public Product insert(Product product) {
+		return productRepository.save(product);
+	}
 
 }
