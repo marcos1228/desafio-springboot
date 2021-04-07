95a58958ad6d15f71e991b40a23b7ce692475947
Marcos Barbosa Evangelista <evangelistamarcos99@gmail.com>
2021-04-07 08:42:02 -0300

    Criando o projeto desafio-springboot

---
+++ src/main/java/com/desafiospringboot/DesafioSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.desafiospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSpringbootApplication.class, args);
+	}
+
+}
